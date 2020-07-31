3f70e4ff82814bc681948e6663878cc715dadc3e
micmor-m <micmor.m@gmail.com>
2020-07-31 01:14:53 +0000

    promised version

---
+++ index2.js
@@ -0,0 +1,24 @@
+// const { fetchMyIP } = require('./iss_promised');
+// const { fetchCoordsByIP } = require('./iss_promised');
+// const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+//cal the req function and pass the function itself as call back
+// fetchMyIP()
+//   //the order of the 3 then below matter if called before fetchISSFlyOverTimes generate error.
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then((body) => console.log(body));
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  });
